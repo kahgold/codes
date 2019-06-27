fc9127511cf1be4e46889e9622aa050b27bba9e3
kahgold <52286387+kahgold@users.noreply.github.com>
2019-06-27 13:49:37 -0300

    Add files via upload

---
+++ formula_bhaskara.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 13:27:32 2019
+
+@author: katia
+"""
+valor = input()
+valor = map(float, valor.split())
+A, B, C = valor
+
