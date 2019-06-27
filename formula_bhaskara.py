e0c1caa81ef256f80205869b65517227524b31c6
kahgold <52286387+kahgold@users.noreply.github.com>
2019-06-27 14:03:14 -0300

    Add files via upload

--- formula_bhaskara.py
+++ formula_bhaskara.py
@@ -5,7 +5,21 @@ Created on Thu Jun 27 13:27:32 2019
 
 @author: katia
 """
+import math
+
+
 valor = input()
 valor = map(float, valor.split())
 A, B, C = valor
 
+delta = B ** 2 - (4 * A * C)
+
+raiz1 = (- B + math.sqrt(delta)) / (2 * A)
+raiz2 = (- B - math.sqrt(delta)) / (2 * A)
+
+print("R1 = {:.5f}".format(raiz1))
+print("R2 = {:.5f}".format(raiz2))
+if raiz1 < 0 and raiz2 < 0:
+    print("Impossivel calcular")
+if raiz1 / 0 and raiz2 / 0:
+    print("Impossivel calcular")
