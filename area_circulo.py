fed66534e00538b270a680e9ec45d2ff12fefef5
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-01 11:11:24 -0300

    Add files via upload

---
+++ area_circulo.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+
+raio = float(input()) #informa o raio
+pi = 3.14159 
+area = pi * (raio**2) #calcula a área do círculo
+print("A=%.4f" %area) #imprime a áre, com 2 casas decimais
