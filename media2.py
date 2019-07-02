50f9f09e0eafcb198cb8f187513cf59373f7698e
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-02 11:13:32 -0300

    Add files via upload

---
+++ media2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 16:28:27 2019
+
+@author: katia
+"""
+
+A = float(input())
+B = float(input())
+C = float(input())
+MEDIA = ((A *2) + (B * 3) + (C * 5)) / 10
+print('MEDIA = %.1f' %MEDIA)
