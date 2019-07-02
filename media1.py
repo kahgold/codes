3a10379d85ab4165801e2b34e0ea8eb0ac6919a4
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-02 11:13:15 -0300

    Add files via upload

---
+++ media1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 16:23:56 2019
+
+@author: katia
+"""
+
+A = float(input())
+B = float(input())
+MEDIA = ((A * 3.5) + (B * 7.5)) / 11
+print('MEDIA = %.5f' %MEDIA)
