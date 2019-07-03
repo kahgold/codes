68db21f1307bd02f4562e6639974b13f35385b03
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-03 15:51:26 -0300

    Add files via upload

---
+++ esfera.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 11:31:00 2019
+
+@author: katia
+"""
+
+pi = 3.14159
+raio = float(input()) #informa o raio da esfera
+volume = (4/3) * pi * (raio**3) #calcula o volume da esfera
+print("VOLUME = %.3f" %volume) #imprime o volume da esfera, com 3 casas decimais
