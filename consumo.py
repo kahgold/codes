684f7377bbf67bcdad3cba6b212dc07d40e6f111
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-06 13:43:42 -0300

    Add files via upload

---
+++ consumo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 17:19:11 2019
+
+@author: katia
+"""
+
+X = int(input())
+Y = float(input())
+
+res = X / Y
+print('%.3f km/l'%res)
