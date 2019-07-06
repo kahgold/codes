7a647b30efd2fce9e0d0db432e3102d58ca47220
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-06 13:43:57 -0300

    Add files via upload

---
+++ poligonos_regulares_simples.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 14:02:41 2019
+
+@author: katia
+"""
+X = input()
+X = map(int, X.split())
+N, L = X
+
+
+P = N * L
+print(P)
