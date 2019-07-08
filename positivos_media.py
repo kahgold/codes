c803921bf0ab651629cd542a68782ed48e85ee27
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-08 12:51:37 -0300

    Add files via upload

---
+++ positivos_media.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 12:33:12 2019
+
+@author: katia
+"""
+numbers = []
+for i in range(6):
+    N = float(input())
+    numbers.append(numbers)
+
+lista = []
+
+if N in numbers > 0:
+    lista.append(lista)
+
+if N in numbers > 0:
+    media = N / 6
+
+
+print("{} valores positivos", lista)
+print(media)
