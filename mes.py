6218f6f778c5caee5d79b7f033ae4a23a93f8240
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-08 12:21:13 -0300

    Add files via upload

---
+++ mes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 12:10:02 2019
+
+@author: katia
+"""
+N = int(input())
+
+
+meses = {1: {'January'}, 2: {'February'}, 3: {'March'}, 4: {'April'},
+         5: {'May'}, 6: {'June'}, 7: {'July'}, 8: {'August'},
+         9: {'September'}, 10: {'October'}, 11: {'November'}, 12: {'December'}}
+
+print(meses.[key])
