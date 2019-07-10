b3075723b04980bbed806d6c194346ba952c7715
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-10 12:19:06 -0300

    Add files via upload

---
+++ quadrado_cubo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 11:21:34 2019
+
+@author: katia
+"""
+N = int(input())
+
+for i in range(1, N + 1):
+    # _input = input()
+    # _input = map(float, _input.split())
+    print("{} {} {}".format(i, i**2, i**3))
