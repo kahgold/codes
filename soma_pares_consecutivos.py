0eb0a4029e7da2355716042b382a0947d6918310
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-15 17:01:03 -0300

    Add files via upload

---
+++ soma_pares_consecutivos.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 16:52:41 2019
+
+@author: katia
+"""
+X = int(input())
+while X != 0:
+    X = int(input())
+
+print(X + 2)
