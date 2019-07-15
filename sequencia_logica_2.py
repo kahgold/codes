a82104e402292ea73623459cb689849a0dc269b3
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-15 15:23:30 -0300

    Add files via upload

---
+++ sequencia_logica_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 15:17:51 2019
+
+@author: katia
+"""
+
+_input = input()
+_input = map(int, _input.split())
+X, Y = _input
+
+
+for i in range(1, Y):
+    print(i)
