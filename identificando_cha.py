44af64e11b5a0ff3fd92f7fd23e98f0e6648b5f4
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-16 12:10:09 -0300

    Add files via upload

---
+++ identificando_cha.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 12:00:28 2019
+
+@author: katia
+"""
+tea = {{1: 'cha branco'}, {2: 'cha verde'}, {3: 'cha preto'},
+       {4: 'cha de ervas'}}
+
+T = int(input())
+_input = input()
+_input = map(float, _input.split())
+A, B, C, D, E = _input
