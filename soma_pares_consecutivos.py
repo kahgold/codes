4db8d82788cba90ade472bdf92af120123d721c8
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-16 14:37:44 -0300

    Add files via upload

--- soma_pares_consecutivos.py
+++ soma_pares_consecutivos.py
@@ -4,9 +4,16 @@
 Created on Mon Jul 15 16:52:41 2019
 
 @author: katia
+1159
 """
-X = int(input())
-while X != 0:
-    X = int(input())
 
-print(X + 2)
+while 1:
+    _input = int(input())
+    if _input == 0:
+        break
+    _input = _input if _input % 2 == 0 else _input + 1
+    soma = _input
+    for i in range(4):
+        _input += 2
+        soma += _input
+    print(soma)
