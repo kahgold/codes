dba014910e411b9790343ccc49c7ed4cb430a4ea
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-17 12:10:23 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 17 11:55:22 2019
+
+@author: katia
+1078
+"""
+N = int(input())
+
+for i in range(1, 11):
+    print('{} X {} = {}'.format(i, N, i * N))
