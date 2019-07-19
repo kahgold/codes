d3df75ff2c589358251f639698a3132add4dfb8f
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-19 13:38:13 -0300

    Add files via upload

---
+++ Ho_Ho_Ho.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 13:33:17 2019
+
+@author: katia
+1759
+"""
+N = int(input())
+
+for i in range(N):
+    print("Ho", end=" ")
