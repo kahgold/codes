1bae8bfd978776db2e15b5b0f098fdb3cc7df351
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-23 11:47:22 -0300

    Add files via upload

---
+++ resposta_Theon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 11:37:58 2019
+
+@author: katia
+1858
+"""
+N = int(input())
+_input = input()
+_input = map(int, _input.split())
+N = _input
+
+#  for i in range(1, N):
