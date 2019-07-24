ea029dbdff39bc9df44cc4a6a36eddf4881992db
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-24 12:46:51 -0300

    Add files via upload

---
+++ fuso_horario.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 12:39:10 2019
+
+@author: katia
+2057
+"""
+_input = input()
+_input = map(int, _input.split())
+S, T, F = _input
