0aea797c3fafe598da65a5ea918d9d687bfdb5ea
kahgold <52286387+kahgold@users.noreply.github.com>
2019-07-24 12:50:37 -0300

    Add files via upload

---
+++ duas_notas.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 12:47:38 2019
+
+@author: katia
+"""
+lst = [2, 5, 10, 20, 50, 100]
+
+_input = input()
+_input = map(int, _input.split())
+N, M = _input
